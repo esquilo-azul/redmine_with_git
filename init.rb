334fc851997581c1cbd3d985f1eed472bdc8c383
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2018-03-15 15:08:46 -0300

    "redmine_git_hosting" plugin dependency.

--- init.rb
+++ init.rb
@@ -9,4 +9,6 @@ Redmine::Plugin.register :redmine_with_git do
   version '0.0.0'
   url 'http://172.18.4.200/redmine/projects/redmine'
   author_url 'http://172.18.4.200/redmine/projects/seinf-ap'
+
+  requires_redmine_plugin :redmine_git_hosting, version_or_higher: '1.0.4'
 end
