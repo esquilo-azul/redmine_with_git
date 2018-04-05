364b9bdcdf707a010f12ddbd44768bad1143b454
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2018-04-05 09:50:59 -0300

    Bump to '0.1.0'.

--- init.rb
+++ init.rb
@@ -6,7 +6,7 @@ Redmine::Plugin.register :redmine_with_git do
   name 'Redmine + Git'
   author 'Eduardo Henrique Bogoni'
   description 'Redmine + Git'
-  version '0.0.0'
+  version '0.1.0'
   url 'http://172.18.4.200/redmine/projects/redmine'
   author_url 'http://172.18.4.200/redmine/projects/seinf-ap'
 
