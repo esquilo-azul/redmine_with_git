d1f5e385a359fdcf0dfc62b01df24ed9e90f6ce3
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2018-07-20 15:05:47 -0300

    Declare dependencies.

---
+++ config/initializers/000_dependencies.rb
@@ -0,0 +1,9 @@
+Redmine::Plugin.post_register :redmine_nonproject_modules do
+  # Source: https://github.com/esquilo-azul/redmine_plugins_helper
+  requires_redmine_plugin(:redmine_plugins_helper, version_or_higher: '0.3.0')
+  # Source: https://github.com/jbox-web/redmine_bootstrap_kit.git
+  # Note: redmine_bootstrap_kit is a redmine_git_hosting's dependency.
+  requires_redmine_plugin(:redmine_bootstrap_kit, version_or_higher: '0.2.5')
+  # Source: https://github.com/jbox-web/redmine_git_hosting.git
+  requires_redmine_plugin(:redmine_git_hosting, version_or_higher: '1.2.3')
+end
