38e2dc4fe86a31fadfc47f60de7e60127834bd7a
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2019-03-18 16:02:17 -0300

    Update "redmine_plugins_helper" to '0.4.0'.

--- config/initializers/000_dependencies.rb
+++ config/initializers/000_dependencies.rb
@@ -1,6 +1,6 @@
 Redmine::Plugin.post_register :redmine_with_git do
   # Source: https://github.com/esquilo-azul/redmine_plugins_helper
-  requires_redmine_plugin(:redmine_plugins_helper, version_or_higher: '0.3.0')
+  requires_redmine_plugin(:redmine_plugins_helper, version_or_higher: '0.4.0')
   # Source: https://github.com/jbox-web/redmine_bootstrap_kit.git
   # Note: redmine_bootstrap_kit is a redmine_git_hosting's dependency.
   requires_redmine_plugin(:redmine_bootstrap_kit, version_or_higher: '0.2.5')
