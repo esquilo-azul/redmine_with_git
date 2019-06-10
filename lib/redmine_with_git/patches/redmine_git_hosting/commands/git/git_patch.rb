cf55040c1bea14406968159f68fcf361e3941496
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2019-06-09 21:57:15 -0300

    Fix RedmineWithGit::Commands::Git.git_version for Git '2.X'.

---
+++ lib/redmine_with_git/patches/redmine_git_hosting/commands/git/git_patch.rb
@@ -0,0 +1,27 @@
+module RedmineWithGit
+  module Patches
+    module RedmineGitHosting
+      module Commands
+        module GitPatch
+          def self.included(base)
+            base.send(:include, InstanceMethods)
+            base.alias_method_chain :git_version, :redmine_with_git
+          end
+        end
+
+        module InstanceMethods
+          def git_version_with_redmine_with_git
+            sudo_git('--version')
+          rescue RedmineGitHosting::Error::GitoliteCommandException => e
+            logger.error("Can't retrieve Git version: #{e.output}")
+            'unknown'
+          end
+        end
+      end
+    end
+  end
+end
+
+patch = ::RedmineWithGit::Patches::RedmineGitHosting::Commands::GitPatch
+target = ::RedmineGitHosting::Commands::Git
+target.send(:include, patch) unless target.included_modules.include?(patch)
