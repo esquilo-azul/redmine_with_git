d88b44ad12161bcb08ff6c8780832f3b05bd0ef7
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2019-08-27 15:45:31 -0300

    Plugin "redmine_nonproject_modules" '>= 0.3.0'.

--- config/initializers/000_dependencies.rb
+++ config/initializers/000_dependencies.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 Redmine::Plugin.post_register :redmine_with_git do
-  # Source: https://github.com/esquilo-azul/redmine_plugins_helper
-  requires_redmine_plugin(:redmine_plugins_helper, version_or_higher: '0.5.2')
+  # Source: https://github.com/esquilo-azul/redmine_nonproject_modules
+  requires_redmine_plugin(:redmine_nonproject_modules, version_or_higher: '0.3.0')
   # Source: https://github.com/jbox-web/redmine_bootstrap_kit.git
   # Note: redmine_bootstrap_kit is a redmine_git_hosting's dependency.
   requires_redmine_plugin(:redmine_bootstrap_kit, version_or_higher: '0.2.5')
