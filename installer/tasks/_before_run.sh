3a9716f9858f470d8891d03c823ad2b0460697fd
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2019-09-11 09:59:17 -0300

    installer/tasks/redmine_git_hosting.

--- installer/tasks/_before_run.sh
+++ installer/tasks/_before_run.sh
@@ -42,3 +42,4 @@ export REDMINE_WITH_GIT_TEMPLATE_ROOT="${REDMINE_WITH_GIT_INSTALL_ROOT}/template
 
 # Task dependencies
 taskeiro_add_dependency redmine_bundle redmine_with_git_bundle_requirements
+taskeiro_add_dependency development redmine_git_hosting
---
+++ installer/tasks/redmine_git_hosting.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -u
+set -e
+
+function task_condition {
+  return 0
+}
+
+function task_dependencies {
+  echo gitolite_rc redmine_gitolite_sudoer redmine_git_hosting_settings apt_ruby python_two
+}
