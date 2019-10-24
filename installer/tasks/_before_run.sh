801b769bd8ae4ad1c6dedfa5818008d9162f6f18
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2019-10-24 16:02:57 -0300

    installer/tasks/gitolite.rc.

--- installer/tasks/_before_run.sh
+++ installer/tasks/_before_run.sh
@@ -3,5 +3,9 @@ export gitolite_user=git
 export gitolite_user_home=/var/lib/git
 export redmine_git_hosting_ssh_key_name=redmine_git_hosting_id
 
+# Auxiliary settings
+export REDMINE_WITH_GIT_INSTALL_ROOT="$(dirname "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )")"
+export REDMINE_WITH_GIT_TEMPLATE_ROOT="${REDMINE_WITH_GIT_INSTALL_ROOT}/template"
+
 # Task dependencies
 taskeiro_add_dependency redmine_bundle redmine_with_git_bundle_requirements
---
+++ installer/tasks/gitolite_rc.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -u
+set -e
+
+gitolite_rc_file="$gitolite_user_home/.gitolite.rc"
+
+function gitolite_rc_template {
+  programeiro /template/apply "${REDMINE_WITH_GIT_TEMPLATE_ROOT}/gitolite.rc"
+}
+
+function task_dependencies {
+  echo gitolite_setup
+}
+
+function task_condition {
+  temprc="$(sudo -u "$gitolite_user" mktemp)"
+  sudo -u "$gitolite_user" cp "$gitolite_user_home/.gitolite.rc" "$temprc"
+  sudo -u "$gitolite_user" chmod 777 "$temprc"
+  if [ "$(gitolite_rc_template | programeiro /text/diff_stdin_file "$temprc" )" -ne 0 ]; then
+    return 1
+  fi
+}
+
+function task_fix {
+  gitolite_rc_template | sudo -u "$gitolite_user" tee "$gitolite_user_home/.gitolite.rc" > /dev/null
+  programeiro /redmine/installer/triggers/set 'redmine_git_hosting_rescue'
+}
