7555931a0233752698a5e6e6ecb6e1d765713ee8
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2019-10-24 18:58:08 -0300

    installer/tasks/redmine_gitolite_sudoer.

---
+++ installer/tasks/redmine_gitolite_sudoer.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+set -u
+set -e
+
+SUDOER_FILE="/etc/sudoers.d/$(programeiro /rails/user)_redmine_with_git"
+
+function sudoers_file_copy_file() {
+  TMPFILE="$(mktemp)"
+  sudo cp "$SUDOER_FILE" "$TMPFILE"
+  sudo chmod og+r "$TMPFILE"
+  printf "$TMPFILE\n"
+}
+
+function task_dependencies {
+  echo gitolite_user
+}
+
+export -f task_dependencies
+
+function task_condition {
+  if [ "$(programeiro /linux/sudo_file_exists "root" "$SUDOER_FILE")" != '0' ]; then
+    return 1
+  fi
+  export rails_user="$(programeiro /rails/user)"
+  SUDOERS_FILE_COPY="$(sudoers_file_copy_file)"
+  result=$(programeiro /template/apply "${REDMINE_WITH_GIT_TEMPLATE_ROOT}/redmine_user_sudoer" | programeiro /text/diff_stdin_file "$SUDOERS_FILE_COPY")
+  sudo rm -f "$SUDOERS_FILE_COPY"
+  if [ "$result" != '0' ]; then
+    return 1
+  fi
+  if [ "$(sudo stat -c "%a" "$SUDOER_FILE")" != '440' ]; then
+    return 1
+  fi
+}
+export -f task_condition
+
+function task_fix {
+  set -u
+  set -e
+  export rails_user="$(programeiro /rails/user)"
+  programeiro /template/apply "$REDMINE_WITH_GIT_TEMPLATE_ROOT/redmine_user_sudoer" | sudo tee "$SUDOER_FILE" > /dev/null
+  sudo chmod 440 "$SUDOER_FILE"
+}
+export -f task_fix
