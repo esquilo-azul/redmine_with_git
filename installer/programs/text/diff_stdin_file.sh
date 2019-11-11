31718d71f5429396e5e3b45e5647d5b03cc26f71
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2019-11-11 16:41:12 -0300

    installer/programs/text/diff_stdin_file: check for file existence.

--- installer/programs/text/diff_stdin_file.sh
+++ installer/programs/text/diff_stdin_file.sh
@@ -3,8 +3,12 @@
 set -u
 set -e
 
-set +e
-diff "/dev/stdin" "$1" > /dev/null
-result=$?
-set -e
-echo $result
+if [ -f "$1" ]; then
+  set +e
+  diff "/dev/stdin" "$1" > /dev/null
+  result=$?
+  set -e
+  echo $result
+else
+  echo 9999
+fi
