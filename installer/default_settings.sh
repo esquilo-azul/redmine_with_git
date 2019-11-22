b49befe1303aa024bd127aab5141867ac9d3568e
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2019-11-22 15:11:27 -0300

    installer/default_settings: moved variables from "installer/tasks/_before_run".

---
+++ installer/default_settings.sh
@@ -0,0 +1,47 @@
+# Auxilary functions
+function sanitize_boolean_var {
+  if [ "$1" != 'false' -a "$1" != 'true' ]; then
+    if [ -n "$1" ]; then
+      echo 'true'
+    else
+      echo 'false'
+    fi
+  else
+    echo "$1"
+  fi
+}
+
+function set_by_boolean {
+  local value="${!1}"
+  if [ "$(sanitize_boolean_var "$value")" == 'true' ]; then
+    export $2=$3
+  else
+    export $2=$4
+  fi
+}
+
+# Default settings
+export address_https=false
+export address_host=localhost
+export address_port=
+set_by_boolean address_https address_scheme https http
+export address_server="$address_host"
+if [ -n "$address_port" ]; then
+  export address_server="$address_server:$address_port"
+fi
+export git_repositories_hierarchical_organisation=true
+set_by_boolean git_repositories_hierarchical_organisation git_repositories_unique_repo_identifier \
+  false true
+export gitolite_user=git
+export gitolite_user_home=/var/lib/git
+export redmine_git_hosting_ssh_key_name=redmine_git_hosting_id
+
+# Auxiliary settings
+export REDMINE_WITH_GIT_INSTALL_ROOT="$(dirname "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )")"
+export REDMINE_WITH_GIT_TEMPLATE_ROOT="${REDMINE_WITH_GIT_INSTALL_ROOT}/template"
+
+# Redmine configuration
+if [ ! -v 'REDMINE_CONFIGURATION_EXTRA' ]; then
+  export REDMINE_CONFIGURATION_EXTRA=''
+fi
+REDMINE_CONFIGURATION_EXTRA+="  scm_xiolite_command: /usr/bin/git\n"
