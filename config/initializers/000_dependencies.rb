a3043d03d3d3ed285eb8f3ef671e20ba53cad205
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2020-01-18 14:46:15 -0300

    Plugin dependency "redmine_installer" ('0.9.0').

--- config/initializers/000_dependencies.rb
+++ config/initializers/000_dependencies.rb
@@ -8,4 +8,9 @@ Redmine::Plugin.post_register :redmine_with_git do
   requires_redmine_plugin(:redmine_bootstrap_kit, version_or_higher: '0.2.5')
   # Source: https://github.com/jbox-web/redmine_git_hosting.git
   requires_redmine_plugin(:redmine_git_hosting, version_or_higher: '1.2.3')
+
+  # Source: https://github.com/esquilo-azul/redmine_installer
+  if ::Redmine::Plugin.registered_plugins.keys.include?(:redmine_installer)
+    requires_redmine_plugin(:redmine_installer, version_or_higher: '0.9.0')
+  end
 end
