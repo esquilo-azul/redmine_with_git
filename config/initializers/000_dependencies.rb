0b149b87a922cb6b87f0e8a46a80768ab2169e3f
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2020-02-21 14:39:52 -0300

    Plugin "redmine_installer": update to '>= 0.10.0'.

--- config/initializers/000_dependencies.rb
+++ config/initializers/000_dependencies.rb
@@ -11,6 +11,6 @@ Redmine::Plugin.post_register :redmine_with_git do
 
   # Source: https://github.com/esquilo-azul/redmine_installer
   if ::Redmine::Plugin.registered_plugins.keys.include?(:redmine_installer)
-    requires_redmine_plugin(:redmine_installer, version_or_higher: '0.9.0')
+    requires_redmine_plugin(:redmine_installer, version_or_higher: '0.10.0')
   end
 end
