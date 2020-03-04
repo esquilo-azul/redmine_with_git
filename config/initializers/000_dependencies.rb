81f8c93b1169d378d7b32c810c5b6cd4df614ab7
Eduardo Henrique Bogoni <eduardo.bogoni@trf1.jus.br>
2020-03-04 14:58:18 -0300

    Plugin "redmine_git_hosting": remote address to https://github.com/esquilo-azul/redmine_git_hosting.

--- config/initializers/000_dependencies.rb
+++ config/initializers/000_dependencies.rb
@@ -6,7 +6,7 @@ Redmine::Plugin.post_register :redmine_with_git do
   # Source: https://github.com/jbox-web/redmine_bootstrap_kit.git
   # Note: redmine_bootstrap_kit is a redmine_git_hosting's dependency.
   requires_redmine_plugin(:redmine_bootstrap_kit, version_or_higher: '0.2.5')
-  # Source: https://github.com/jbox-web/redmine_git_hosting.git
+  # Source: https://github.com/esquilo-azul/redmine_git_hosting
   requires_redmine_plugin(:redmine_git_hosting, version_or_higher: '1.2.3.1')
 
   # Source: https://github.com/esquilo-azul/redmine_installer
