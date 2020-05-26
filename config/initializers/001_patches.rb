dea51f44ba0a15588b080639b2ab581f3de83b71
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2020-05-25 22:25:01 -0300

    RedmineWithGit: do not apply patches to RedmineGitHosting >= '4.0.0'.

--- config/initializers/001_patches.rb
+++ config/initializers/001_patches.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
 
+apply_patches_version_limit = ::Gem::Version.new('4.0.0')
+redmine_git_hosting_version = ::Gem::Version.new(
+  ::Redmine::Plugin.registered_plugins[:redmine_git_hosting].version
+)
+
+return unless redmine_git_hosting_version < apply_patches_version_limit
+
 require 'redmine_with_git/patches/redmine_git_hosting/cache/database_patch'
 require 'redmine_with_git/patches/redmine_git_hosting/commands/git/git_patch'
