f9c5992f3c3e7b5e109ab78f21a8565bb03b2f80
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2025-09-01 17:30:38 -0300

    Installer#Task[redmine_with_git_bundle_requirements]: adapt to "Ubuntu" '24.04'.

--- installer/tasks/redmine_with_git_bundle_requirements.sh
+++ installer/tasks/redmine_with_git_bundle_requirements.sh
@@ -3,8 +3,18 @@
 set -u
 set -e
 
-REDMINE_WITH_GIT_PACKAGES=(build-essential libcurl4-openssl-dev libssh2-1 libssh2-1-dev cmake \
+function greater_or_equal_noble() {
+  dpkg --compare-versions "$(programeiro /ubuntu/version)" ge '24.04'
+}
+
+REDMINE_WITH_GIT_PACKAGES=(build-essential libcurl4-openssl-dev libssh2-1-dev cmake \
   libgpg-error-dev libssl-dev)
+
+if greater_or_equal_noble ; then
+  REDMINE_WITH_GIT_PACKAGES+=(libssh2-1t64)
+else
+  REDMINE_WITH_GIT_PACKAGES+=(libssh2-1)
+fi
 
 function task_condition {
   package_installed apt "${REDMINE_WITH_GIT_PACKAGES[@]}"
